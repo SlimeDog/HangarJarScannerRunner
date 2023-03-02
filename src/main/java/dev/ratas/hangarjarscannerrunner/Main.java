65f582e6a03aaede6b40a293d14f014379d010ba
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2023-03-02 09:11:53 +0000

    Initial working commit

---
+++ src/main/java/dev/ratas/hangarjarscannerrunner/Main.java
@@ -0,0 +1,54 @@
+package dev.ratas.hangarjarscannerrunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import io.papermc.hangar.scanner.HangarJarScanner;
+import io.papermc.hangar.scanner.model.ScanResult;
+
+public final class Main {
+
+    private static InputStream getInputStream(String filename) {
+        try {
+            return new FileInputStream(new File(filename));
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private static void showResults(List<ScanResult> results) {
+        for (ScanResult result : results) {
+            System.out.println("" + result);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HELLO with args " + String.join(" ", args));
+        HangarJarScanner scanner = new HangarJarScanner();
+        for (String jarName : args) {
+            System.out.println("Checking " + jarName);
+            InputStream jar;
+            try {
+                jar = getInputStream(jarName);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Could not load JAR:");
+                e.printStackTrace();
+                continue;
+            }
+            List<ScanResult> results;
+            try {
+                results = scanner.scanJar(jar, jarName);
+            } catch (IOException e) {
+                System.err.println("Issue loading JAR:");
+                e.printStackTrace();
+                continue;
+            }
+            showResults(results);
+        }
+    }
+
+}
